6693df121d80475414d78df8346986a2cf36d6f2
Cesar Augusto Alvarez Mantari <cesar.alvarez@globant.com>
2022-04-20 10:33:10 -0500

    Greetings added

--- java2/test2.java
+++ java2/test2.java
@@ -2,7 +2,9 @@ public class test2{
 
 	public static void main (arg[]){
 	
-		system.out.println("Hola ps again");	
+		system.out.println("Hola ps again");
+		system.out.println("A ver que pasa aqui");
+		system.out.println("Todo tranqui");
 
 
 	}
